dab619b1fc3c250c7cf36765f2f15a69ea76210b
Simon-157 <boatengsimonjnr157@gmail.com>
2024-06-15 15:32:14 +0000

    modified passport oauth for auth user ✗

--- src/middleware/sessionMiddleware.ts
+++ src/middleware/sessionMiddleware.ts
@@ -3,13 +3,15 @@ import { redisClient } from "../config/redis";
 import { Session, SessionOptions } from "express-session";
 
 export const sessionMiddleware = {
-  secret: "secret",
+  secret: process.env.SESSION_SECRET || "wanderer2024",
   resave: false,
   saveUninitialized: true,
   store: new RedisStore({ client: redisClient }),
   cookie: {
     maxAge: 72 * 60 * 60 * 1000,
+    httpOnly:true,
+    secure: process.env.NODE_ENV == "production",
+    sameSite: "none",
+
   },
 } as SessionOptions;
-
-
