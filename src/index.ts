8b79cd6665c07a49136831b62a3569c2e3582775
Simon-157 <boatengsimonjnr157@gmail.com>
2024-06-15 16:53:58 +0000

    modified passport oauth for auth user ✗

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,20 @@ const app = express();
 // middleware stack
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+app.set("trust proxy", 1);
 app.use(cors(corsMiddleware));
 app.use(session(sessionMiddleware));
-// authentication stack
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(httpLogger);
 
 // routes stack
